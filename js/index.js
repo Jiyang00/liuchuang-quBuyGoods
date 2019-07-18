8a49b3f241223b6fe7555e53d904d87f3cd7ff81
zhao <1164375084@qq.com>
2019-07-18 09:20:06 +0800

    趣买货官网完成

---
+++ js/index.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    // var mySwiper = new Swiper('.swiper-container', {
+    //     direction: 'horizontal', // 垂直切换选项
+    //     loop: true, // 循环模式选项
+    //     speed: 2000,
+
+    //     // 如果需要分页器
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //     },
+    //     autoplay: {
+    //         delay: 3000
+    //     },
+    // })
+    var mySwiper = new Swiper('.swiper-container',{
+        loop: true,
+        direction: 'horizontal', // 垂直切换选项
+        speed: 2000,
+        autoplay : 3000,
+      });  
+}
